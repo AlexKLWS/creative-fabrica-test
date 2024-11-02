709485809f0884099b39bc68ca9ffb2bef341ded
Alex Korzh <alexkorzh7@gmail.com>
2024-11-01 17:59:36 -0700

    Add constants

---
+++ creative-fabrica-test-fe/src/constants/testIds.ts
@@ -0,0 +1,6 @@
+export const LOGOUT_BUTTON_TEST_ID = "logout-button";
+export const PRODUCT_LOADER_CARD_TEST_ID = "posting-loader-card";
+export const PRODUCT_CARD_TEST_ID = "posting-card";
+export const BOOKMARK_BUTTON_TEST_ID = "bookmark-button";
+export const BOOKMARK_BUTTON_ICON_TEST_ID = "bookmark-button-icon";
+export const BOOKMARK_BUTTON_LOADER_TEST_ID = "bookmark-button-loader";
